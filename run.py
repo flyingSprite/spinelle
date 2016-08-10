267424ac0bea8db0e7a1f8de66093fa2cf722751
Quesle <eneblue@126.com>
2016-08-11 00:38:11 +0800

    Modify run.py code, support arguments

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 
+import sys
 
 from scrapy.conf import settings
 from scrapy.crawler import Crawler, CrawlerProcess
@@ -6,17 +7,18 @@ from spinelle.spiders.asahi import AsahiSpider
 from spinelle.spiders.sankei import SankeiSpider
 from spinelle.spiders.huanqiu import HuanqiuSpider
 
+from service.aliyun_cms import run
 
 from conf.settings import MISSION_MESSAGE
 from utility.mongo import MongoService
 
 
-class Process(object):
+class CrawlService(object):
 
     def __init__(self):
-        service = MongoService()
+        self.mongoService = MongoService()
         self.crawlProcess = CrawlerProcess(settings=settings)
-        self.collection = service.collection('crawl')
+        self.collection = self.mongoService.collection('crawl')
 
     def process(self):
         self.crawlProcess.crawl(AsahiSpider)
@@ -36,5 +38,31 @@ class Process(object):
         self.collection.insert_one(crawl_record)
 
 
-process = Process()
-process.start()
+class Service(object):
+    service = None
+
+    def __init__(self):
+        pass
+
+    def start_crawl(self):
+        self.service = CrawlService()
+        self.service.start()
+
+    def start_aliyun_cms(self):
+        run()
+
+
+if __name__ == '__main__':
+    service = Service()
+    length = len(sys.argv)
+    if length == 1:
+        service.start_crawl()
+    elif length == 2:
+        command = sys.argv[1]
+        if command == 'crawl':
+            service.start_crawl()
+        elif command == 'aliyun':
+            service.start_aliyun_cms()
+    else:
+        pass
+
